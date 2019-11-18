acf8cbcb7de29a14e6a382273306f791e9ddd510
Dr. XyClo <sdcarmody@gmail.com>
2019-11-18 02:25:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,5 @@ require 'open-uri'
 
 
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc = Nokogiri::HTML(open("https://flatironschool.com/free-courses/coding-bootcamp-prep"))
 doc.css(".headline-260IBN")
